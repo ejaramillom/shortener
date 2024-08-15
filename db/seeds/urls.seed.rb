d5aa3201b2a3e58a9cc3e15c3ebcca756433f91d
ejaramillom <ejaramilo@hotmail.com>
2024-08-15 14:52:34 -0500

    feat(shortener): Add seed task and load data to the db

---
+++ db/seeds/urls.seed.rb
@@ -0,0 +1,15 @@
+puts 'Data seed in progress...'
+
+ActiveRecord::Base.transaction do
+  Url.destroy_all
+  150.times do |integer|
+    Url.create!([
+      original_url: Faker::Internet.url,
+      shortened_url: SecureRandom.urlsafe_base64(5),
+      title: Faker::Lorem.sentence,
+      counter: Faker::Number.between(from: 0, to: 20)
+    ])
+  end
+end
+
+puts 'Data seed finished...'
